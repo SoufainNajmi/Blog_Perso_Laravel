4a10568f65022c0a790d9e60d42762fd96c9d4fa
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-11 22:32:35 +0200

    CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function store(Request $request, Article $article)
+{
+    $request->validate([
+        'author' => 'required|max:50',
+        'content' => 'required'
+    ]);
+
+    $article->comments()->create($request->all());
+
+    return back()->with('success', 'Commentaire ajouté');
+}
     //
 }
