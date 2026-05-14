d9b2f343b243de1d66900ac022a0c87dbb3ab14d
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-14 20:32:26 +0200

    add aboutSection

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'À propos de moi')
+
+@section('content')
+    <h1>À propos de ce blog</h1>
+
+    <p>
+        Je suis [Ton prénom], développeur débutant, et ce blog est mon terrain de jeu pour apprendre Laravel.
+    </p>
+    <p>
+        Ici je partage mes découvertes, mes projets et mes réflexions sur le développement web.
+    </p>
+    <p>
+        N’hésitez pas à me contacter via [email ou réseau social] !
+    </p>
+@endsection
