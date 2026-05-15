eb64b5cd5b1d118abbe463273442a1c8bb2f4451
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-15 16:04:57 +0200

    view/articles/index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,29 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Liste Articles</title>
-</head>
-<body>
+{{-- resources/views/posts/index.blade.php --}}
+@extends('layouts.app')
 
-<h1>Liste des articles</h1>
+@section('title', 'Accueil Tous les articles')
 
-@foreach($articles as $article)
+@section('content')
+    <h1>Bienvenue sur mon blog</h1>
 
-    <div>
+    @if($articles->isEmpty())
+        <p>Aucun article pour le moment.</p>
+    @else
+        <ul class="post-list">
+            @foreach($articles as $article)
+                <li class="post-item">
+                    <h2>
+                        <a href="{{ route('articles.show', $article->id) }}">
+                            {{ $article->title }}
+                        </a>
+                    </h2>
+                    <p>{{ Str::limit($article->body, 150) }}</p>
+                    <small>Publié le {{ $article->created_at->format('d/m/Y') }}</small>
+                </li>
+            @endforeach
+        </ul>
 
-        <h2>{{ $article->title }}</h2>
-
-        <p>{{ $article->content }}</p>
-
-        <small>
-            Auteur : {{ $article->author }}
-        </small>
-
-        <hr>
-
-    </div>
-
-@endforeach
-
-</body>
-</html>
+        {{ $articles->links() }} {{-- Pagination --}}
+    @endif
+@endsection
