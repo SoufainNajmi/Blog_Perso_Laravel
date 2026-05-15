b6d8f18d21220121e91462b579a3e4a12ffbf01a
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-15 16:05:55 +0200

    update router

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-   //return "hello world";
-    return view('welcome');
-});
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+
+Route::get('/articles/{article}', [App\Http\Controllers\ArticleController::class, 'show'])->name('articles.show');
 Route::get("articles/create", [App\Http\Controllers\ArticleController::class, 'create'])->name('articles.create');
 Route::post("articles", [App\Http\Controllers\ArticleController::class, 'store'])->name('articles.store');
 Route::get("articles", [App\Http\Controllers\ArticleController::class, 'index'])->name('articles.index');
